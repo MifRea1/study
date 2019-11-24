6d7c303ee58711ef6d41a663250542ab60108b33
MifReal <MifReal@list.ru>
2019-11-24 19:22:25 +0400

    More general solution

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,10 +1,26 @@
 module Inatra
   class << self
-    def routes
+    def routes(&block)
+      if block_given?
+        @routes = {}
+        class_exec(&block)
+      else
+        @routes
+      end
     end
 
-    def call(_env)
-      [200, {}, ['Hello World']]
+    def call(env)
+      method = env['REQUEST_METHOD'].downcase.to_sym
+      path = env['PATH_INFO']
+      return [405, {}, 'Method Not Allowed'] unless routes.key?(method)
+      routes[method][path]&.call || [404, {}, 'Not Found']
+    end
+
+    def method_missing(name, *paths, &response)
+      paths.each do |path|
+        routes[name] = {} unless routes.key?(name)
+        routes[name][path] = response
+      end
     end
   end
 end
--- test/exercise/rackup/test.rb
+++ test/exercise/rackup/test.rb
@@ -11,4 +11,16 @@ class RackTest < Test::Unit::TestCase
     assert browser.last_response.ok?
     assert_equal 'Hello World', browser.last_response.body
   end
+
+  def test_status_404_if_route_not_exists
+    browser = Rack::Test::Session.new(Rack::MockSession.new(Inatra))
+    browser.get '/nothing/to/see/here'
+    assert_equal 404, browser.last_response.status
+  end
+
+  def test_status_405_if_method_is_not_allowed
+    browser = Rack::Test::Session.new(Rack::MockSession.new(Inatra))
+    browser.post '/hello'
+    assert_equal 405, browser.last_response.status
+  end
 end
